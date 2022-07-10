b4717fe89b738dbfac74ebd73e7a2d1c4c53af06
songcs0329 <54650110+songcs0329@users.noreply.github.com>
2022-07-10 16:07:28 +0900

    수정

--- scs/flo/3.js
+++ scs/flo/3.js
@@ -1,11 +1,9 @@
 function solution(s)
 {
     let str = s
-    let replaceStr = ""
     for(let i = 0; i < s.length - 1; i++) {
         for(let j = i + 1; j < s.length - 1; j++) {
             if(s[i] === s[j]) {
-                replaceStr = replaceStr + `${s[i]}${s[j]}`
                 str = str.replace(s[i], "")
                 str = str.replace(s[j], "")
             }
