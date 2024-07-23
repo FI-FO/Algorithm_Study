8ca6c25b3b4cd7dc04814e436c16cd1d65efa883
songcs0329 <54650110+songcs0329@users.noreply.github.com>
2024-07-23 16:47:22 +0900

    주석 추가

--- scs/levit/3.js
+++ scs/levit/3.js
@@ -1,6 +1,9 @@
 function solution(n, r, c) {
+		// 대각선 인덱스(0부터 시작)
     let diagIdx = r + c -2;
+		console.log("diagIdx", diagIdx);
 
+		// 대각선 시작점 계산
     let startValue;
 
     if(diagIdx < n) {
@@ -8,16 +11,19 @@ function solution(n, r, c) {
     } else {
         startValue = n * n - (2 * n - 1 - diagIdx) * (2 * n - diagIdx) / 2 + 1;
     }
+		console.log("startValue", startValue);
 
     let value;
 
     if(diagIdx % 2 === 0) {
+				// 짝수줄: 왼쪽 위에서 시작
         if(diagIdx < n) {
             value = startValue + c - 1;
         } else {
             value = startValue + n - r;
         }
     } else {
+				// 홀수줄: 오른쪽 아래에서 시작
         if(diagIdx < n) {
             value = startValue + r - 1;
         } else {
@@ -25,6 +31,7 @@ function solution(n, r, c) {
         }
     }
 
+		console.log("value", value);
 
     return value;
 }
